b103c19ea83bcb77c8c2c8789e7597cc6b1a5d65
Sxzar <contact.sxzar@gmail.com>
2024-08-06 21:08:03 +0300

    feat: Add Player component to manage player information in Tic-Tac-Toe app

---
+++ course-projects/02-tic-tac-toe/src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useState, React } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
+};
+
+export default Player;
