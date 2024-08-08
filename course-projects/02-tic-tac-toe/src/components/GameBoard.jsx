0bb51949b92f6a8bda6f9e902732625b45f000af
Sxzar <contact.sxzar@gmail.com>
2024-08-08 15:53:03 +0300

    feat: Add GameBoard component for displaying the tic-tac-toe game board

---
+++ course-projects/02-tic-tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = () => {
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, cellIndex) => (
+              <li key={cellIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
